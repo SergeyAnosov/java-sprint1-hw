615c31b501ff31c40323ec26d8230bf15a46df61
Sergey Anosov <106556264+SergeyAnosov@users.noreply.github.com>
2022-06-06 09:09:13 +0300

    Update StepTracker.java

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class StepTracker {
     public int stepsGoal = 10000;
+    Scanner scanner = new Scanner(System.in);
 
     public int changeGoal(int steps) {
         stepsGoal = steps;
@@ -17,8 +20,24 @@ public class StepTracker {
     }
 
     class MonthData {
-       int[] stepsPerDay = new int[30];
+       int[] days = new int[30];
     }
 
+    public void setStepValue() {
+        System.out.println("За какой месяц вы хотите ввести шаги? (от 1 до 12)");
+        int month = scanner.nextInt();
+        System.out.println("За какой день вы хотите ввести шаги? (от 1 до 30)");
+        int day = scanner.nextInt();
+        if (day <= 0)             
+        System.out.println("Введите количество шагов (строго больше нуля):");
+        int steps = scanner.nextInt();
+        if (steps <= 0) {
+            System.out.println("введено отрицательное число. программа завершена");
+            return;
+        }
+        
+        monthToData[month-1].days[day-1] = steps;
+    }
+    
 
 }
